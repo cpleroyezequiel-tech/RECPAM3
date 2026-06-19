417ba1b9399b970ff69411f644ee77669b45c2a3
cpleroyezequiel-tech <cpleroyezequiel@gmail.com>
2026-06-19 15:09:38 -0300

    Add files via upload

--- RECPAM3.py
+++ RECPAM3.py
@@ -65,7 +65,8 @@ st.divider()
 ayuda_pegado = (
     "4. Pegue las 3 columnas de Pitágoras (Periodo, Compras, Ventas):" 
     if sistema_origen == "Pitágoras" else 
-    "4. Pegue la tabla completa de SIPF (Pegue la tabla completa de SIPF (Mes, Años, Var%):"
+    "4. Pegue la tabla completa de SIPF (Mes, Años, Var%):"
+)
 
 data_pegada = st.text_area(ayuda_pegado, height=200)
 
