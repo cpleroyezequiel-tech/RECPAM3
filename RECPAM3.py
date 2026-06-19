7099c7c37242bdd97cd290fe785fc7bde0470d50
cpleroyezequiel-tech <cpleroyezequiel@gmail.com>
2026-06-19 15:03:51 -0300

    Add files via upload

--- RECPAM3.py
+++ RECPAM3.py
@@ -65,7 +65,7 @@ st.divider()
 ayuda_pegado = (
     "4. Pegue las 3 columnas de Pitágoras (Periodo, Compras, Ventas):" 
     if sistema_origen == "Pitágoras" else 
-    "4. Pegue la tabla completa de SIPF (Mes, Años, Var%):"
+    "4. Pegue la tabla completa de SIPF ("Evolucion de ventas totales, locales y al exterior >> Enero 2022 a Diciembre 2026 >> Importe AR$ >> Visualizar reporte >> Pegar tabla con titulos hasta el promedio vertical"):"
 )
 
 data_pegada = st.text_area(ayuda_pegado, height=200)
