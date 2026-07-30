e858a28270950fd394ae0fac9cc0ff76fb317a02
cpleroyezequiel-tech <cpleroyezequiel@gmail.com>
2026-07-30 20:44:51 -0300

    Add files via upload

--- RECPAM3.py
+++ RECPAM3.py
@@ -14,7 +14,7 @@ st.markdown("""
 **Instrucciones:**
 1. Indique desde **qué sistema** va a pegar la información (Pitágoras / SIPF). En caso de SIPF descargue la información de "Evolución de ventas totales, locales y al exterior".
 2. Defina el **Mes de Inicio** de su ejercicio.
-3. Ingrese el **Periodo de Cierre** (formato AAAAMM, ej: 202604).
+3. Ingrese el **Periodo de Cierre** (formato AAAAMM, ej: 202606).
 4. Copie la información de su Excel y péguela abajo.
 """)
 
@@ -37,7 +37,7 @@ with col_conf1:
     mes_inicio_num = meses_nombres.index(mes_inicio_nombre) + 1
 
 with col_conf2:
-    raw_input = st.text_input("3. Reexpresar a moneda de (AAAAMM):", value="202605", max_chars=6)
+    raw_input = st.text_input("3. Reexpresar a moneda de (AAAAMM):", value="202606", max_chars=6)
     if len(raw_input) == 6 and raw_input.isdigit():
         mes_destino_input = f"{raw_input[:4]}/{raw_input[4:]}"
     else:
@@ -58,7 +58,7 @@ indices_base = {
     "2025/05": 8714.4871, "2025/06": 8855.5681, "2025/07": 9023.9730, "2025/08": 9193.2441,
     "2025/09": 9384.0922, "2025/10": 9603.8623, "2025/11": 9841.3581, "2025/12": 10121.3715, 
     "2026/01": 10413.0309, "2026/02": 10714.6255, "2026/03": 11077.0608, "2026/04": 11363.0904, 
-    "2026/05": 11607.3937}
+    "2026/05": 11607.3937, "2026/06": 11826.4103}
 
 st.divider()
 
