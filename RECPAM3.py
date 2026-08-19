d669d53abb195ee8953315f09667c623a43acabe
cpleroyezequiel-tech <cpleroyezequiel@gmail.com>
2026-08-19 11:34:43 -0300

    Add files via upload

--- RECPAM3.py
+++ RECPAM3.py
@@ -14,7 +14,7 @@ st.markdown("""
 **Instrucciones:**
 1. Indique desde **qué sistema** va a pegar la información (Pitágoras / SIPF). En caso de SIPF descargue la información de "Evolución de ventas totales, locales y al exterior".
 2. Defina el **Mes de Inicio** de su ejercicio.
-3. Ingrese el **Periodo de Cierre** (formato AAAAMM, ej: 202606).
+3. Ingrese el **Periodo de Cierre** (formato AAAAMM, ej: 202607).
 4. Copie la información de su Excel y péguela abajo.
 """)
 
@@ -58,7 +58,7 @@ indices_base = {
     "2025/05": 8714.4871, "2025/06": 8855.5681, "2025/07": 9023.9730, "2025/08": 9193.2441,
     "2025/09": 9384.0922, "2025/10": 9603.8623, "2025/11": 9841.3581, "2025/12": 10121.3715, 
     "2026/01": 10413.0309, "2026/02": 10714.6255, "2026/03": 11077.0608, "2026/04": 11363.0904, 
-    "2026/05": 11607.3937, "2026/06": 11826.4103}
+    "2026/05": 11607.3937, "2026/06": 11826.4103, "2026/07": 12076.3937}
 
 st.divider()
 
@@ -72,9 +72,9 @@ data_pegada = st.text_area(ayuda_pegado, height=200)
 
 if data_pegada:
     if len(raw_input) < 6:
-        st.warning("Por favor, complete el periodo con 6 dígitos (ejemplo: 202605).")
+        st.warning("Por favor, complete el periodo con 6 dígitos (ejemplo: 202607).")
     elif mes_destino_input not in indices_base:
-        st.error("⚠️ No se admiten reexpresiones anteriores al 2022/01 o posteriores al 2026/05.")
+        st.error("⚠️ No se admiten reexpresiones anteriores al 2022/01 o posteriores al 2026/07.")
     else:
         try:
             # LÓGICA DE LIMPIEZA DE MONTOS
